878292e8ae7ebcaa1453e1a2ae4dc2f1c43661db
Pantherz (V 3 R T) <58447285+overttunic24@users.noreply.github.com>
2021-10-18 09:36:24 -0400

    Update client.lua
    -Added Directions for changing the color of the message in-chat
    -Added Directions for changing the command you do in chat (/discord by default)

--- ChatCommands/client.lua
+++ ChatCommands/client.lua
@@ -1,6 +1,6 @@
-RegisterCommand('discord', function()
+RegisterCommand('discord', function() -- 'discord' is the command you to in chat, feel free to change it 
     TriggerEvent('chat:addMessage', {
-        color = {255,0,0},
+        color = {255,0,0}, --Color of message that appears in FiveM (currently set to red) If you need help with changing the color, look up an RGB color scale
         multiline = true, 
         args = {'[SERVER NAME]','Join Our Discord: Change Me'} --Put Your Servers name in "Server Name", Put your discord invite in "Change Me"
     })
