9198aa84f68687a6e84a6c4fdcf75f8f0981d747
Pantherz (V 3 R T) <58447285+overttunic24@users.noreply.github.com>
2021-10-18 09:39:13 -0400

    Update fxmanifest.lua

--- ChatCommands/fxmanifest.lua
+++ ChatCommands/fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 games {'gta5'}
 
 author 'Pantherz Development'
-description 'Simple Chat Script i made while learning FiveM resource LUA'
-version '1.0.0'
+description 'Highly COnfigureable Chat script i worked on while learning more FiveM development LUA code, as of right now all it does is display a discord invite, might update to do more soon'
+version '1.0.2'
 
 
 client_scripts {
